fee744aab53d535ee6558cf9b51870f5dff11558
Andrew Phimister <157378252+Phim226@users.noreply.github.com>
2026-01-10 00:44:14 +0000

    Streamlined slider creation into single function

--- src/orbit_visualiser/ui/config/orbit_config.py
+++ src/orbit_visualiser/ui/config/orbit_config.py
@@ -26,25 +26,24 @@ class OrbitConfigurer():
         self._config_frame.pack(side = config_frame_placement[0], anchor = config_frame_placement[1], padx = 8, pady = 6)
 
     def build(self) -> None:
-        self._build_eccentricity_slider()
-        self._build_periapsis_slider()
+        self._build_slider("e", "Eccentricity", 2)
+        self._build_slider("rp", "Radius of periapsis", 10)
 
         self._display_frame.pack(side = "top", anchor = "nw", pady = (10, 0))
 
         for i, parameters in enumerate(list(self.orbital_parameters.items())):
             self._build_display(parameters[0], parameters[1], i)
 
-    def _build_eccentricity_slider(self) -> None:
-        self._e_slider = Scale(self._root, from_ = 0, to = 2, resolution = 0.01, length  = 150, orient = "horizontal",
-                              command = partial(self._update_value, "e"), label = "Eccentricity", font=("Segoe UI", 9))
-        self._e_slider.set(self._orbit.e)
-        self._e_slider.pack(side = "top", anchor = "nw")
-
-    def _build_periapsis_slider(self) -> None:
-        self._rp_slider = Scale(self._root, from_ = 0, to = 10, resolution = 0.01, length  = 150, orient = "horizontal",
-                              command = partial(self._update_value, "rp"), label = "Radius of periapsis", font=("Segoe UI", 9))
-        self._rp_slider.set(self._orbit.rp)
-        self._rp_slider.pack(side = "top", anchor = "nw")
+    def _build_slider(self, parameter: str, label: str, upper_lim: int) -> None:
+        slider_name = f"_{parameter}_slider"
+        self.__setattr__(
+            slider_name,
+            Scale(self._root, to = upper_lim, resolution = 0.01, length = 150, orient = "horizontal",
+                  command = partial(self._update_value, parameter), label = label, font = ("Segoe UI", 9))
+        )
+        slider: Scale = self.__getattribute__(slider_name)
+        slider.set(getattr(self._orbit, parameter))
+        slider.pack(side = "top", anchor = "nw")
 
     def _update_value(self, parameter: str, new_val: str) -> None:
         setattr(self._orbit, parameter, new_val)
@@ -64,9 +63,7 @@ class OrbitConfigurer():
         value_label.grid(row = row, column = 1, sticky = "e", padx = (0, 6))
 
     def _update_display(self, parameter: str, display_str: str) -> None:
-        self.__getattribute__(
-            f"_{parameter}_str"
-        ).set(self._format_display_value(getattr(self._orbit, parameter)))
+        self.__getattribute__(f"_{parameter}_str").set(self._format_display_value(getattr(self._orbit, parameter)))
 
     def _format_display_value(self, value: float) -> str:
         if value == np.inf:
