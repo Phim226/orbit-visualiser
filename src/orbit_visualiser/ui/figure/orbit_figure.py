71bd20192b38073a44791018423eacdb12a3f714
Andrew Phimister <157378252+Phim226@users.noreply.github.com>
2026-01-13 13:06:32 +0000

    Can now scroll zoom on plot

--- src/orbit_visualiser/ui/figure/orbit_figure.py
+++ src/orbit_visualiser/ui/figure/orbit_figure.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, Frame
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
 from matplotlib.patches import Circle
@@ -60,6 +61,7 @@ class OrbitFigure():
         self._line, = self._ax.plot(orbit_eq.x(t) , orbit_eq.y(t), color = "black")
 
         self._ax.add_patch(Circle((0, 0), radius = self._body.r, fill = True, zorder = 10))
+        f = self._zoom_factory(self._ax, 1.1)
 
     def _build_canvas(self) -> None:
         self._canvas = FigureCanvasTkAgg(self._fig, master = self._figure_frame)
@@ -77,4 +79,39 @@ class OrbitFigure():
         self._line.set_data(orbit_eq.x(t), orbit_eq.y(t))
 
         self._canvas.draw()
+
+    @staticmethod
+    def _zoom_factory(ax, base_scale = 2.):
+        def zoom_fun(event):
+            # get the current x and y limits
+            cur_xlim = ax.get_xlim()
+            cur_ylim = ax.get_ylim()
+            # set the range
+            cur_xrange = (cur_xlim[1] - cur_xlim[0])*.5
+            cur_yrange = (cur_ylim[1] - cur_ylim[0])*.5
+            xdata = event.xdata # get event x location
+            ydata = event.ydata # get event y location
+            if event.button == 'up':
+                # deal with zoom in
+                scale_factor = 1/base_scale
+            elif event.button == 'down':
+                # deal with zoom out
+                scale_factor = base_scale
+            else:
+                # deal with something that should never happen
+                scale_factor = 1
+                print(event.button)
+            # set new limits
+            ax.set_xlim([xdata - cur_xrange*scale_factor,
+                        xdata + cur_xrange*scale_factor])
+            ax.set_ylim([ydata - cur_yrange*scale_factor,
+                        ydata + cur_yrange*scale_factor])
+            ax.figure.canvas.draw_idle() # force re-draw the next time the GUI refreshes
+
+        fig = ax.get_figure() # get the figure of interest
+        # attach the call back
+        fig.canvas.mpl_connect('scroll_event',zoom_fun)
+
+        #return the function
+        return zoom_fun
 
