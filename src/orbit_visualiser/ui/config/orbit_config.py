c7a61f727bfbcb015f9967fd0bf0f0eafd7b812b
Andrew Phimister <157378252+Phim226@users.noreply.github.com>
2026-01-13 02:35:11 +0000

    Wrote TODO

--- src/orbit_visualiser/ui/config/orbit_config.py
+++ src/orbit_visualiser/ui/config/orbit_config.py
@@ -7,6 +7,7 @@ from orbit_visualiser.core import Orbit, Satellite, CentralBody
 
 # TODO: Display satellite parameters.
 # TODO: Give option to show parameters on the plot (arrows/lines for vectors and distances etc).
+# TODO: Show the correct sign on the infinity symbol for x and y position.
 class OrbitConfigurer():
 
     title_font = ("Orbitron", 16, "bold")
