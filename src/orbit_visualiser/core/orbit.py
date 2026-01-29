525fe3fb7df963d4a480c8ffef7e8289db4ac47b
Andrew Phimister <157378252+Phim226@users.noreply.github.com>
2026-01-29 00:35:28 +0000

    Orbit type and closure checked directly

--- src/orbit_visualiser/core/orbit.py
+++ src/orbit_visualiser/core/orbit.py
@@ -105,9 +105,10 @@ class Orbit():
         self._ra: float = self._apoapsis_erp(e, a)
         self._t_asymp: float = self._asymptote_anomaly_e(e)
         self._turn_angle: float = self._turning_angle_e(e)
-        self._aim_rad: float = self._aiming_radius_erp(e, b)
+        self._aim_rad: float = self._aiming_radius_erp(b)
 
     def _update_orbit_type(self, e: float) -> None:
+        """Determine orbit type and if it's closed using the eccentricity"""
         if e == 0:
             self._orbit_type = "circular"
             self._is_closed = True
@@ -130,44 +131,45 @@ class Orbit():
 
     def _semimajor_axis_erp(self, e: float, rp: float) -> float:
         """Calculate the semimajor axis a using the eccentricity and radius of periapsis"""
-        if e != 1:
+        if self._orbit_type != "parabolic":
             return rp/(1 - e)
 
         return np.inf
 
     def _semiminor_axis_erp(self, e: float, a: float) -> float:
-        """Calculate the semiminor axis b using the eccentricity and radius of periapsis"""
-        if e > 1:
+        """Calculate the semiminor axis b using the eccentricity and semi-major axis"""
+        if self._orbit_type == "hyperbolic":
             return a*np.sqrt(e**2 - 1)
 
-        elif e < 1:
+        elif self._is_closed:
             return a*np.sqrt(1 - e**2)
 
         return np.inf
 
     def _apoapsis_erp(self, e: float, a: float) -> float:
-        """Calculate the radius of apoapsis ra using the eccentricity and radius of periapsis"""
-        if e != 1:
+        """Calculate the radius of apoapsis ra using the eccentricity and semi-major axis"""
+        if self._orbit_type != "parabolic":
             return a*(1 + e)
 
         return np.inf
 
     def _asymptote_anomaly_e(self, e: float) -> float:
-        """Calculate the true anomaly of the asymptote for hyperbolic orbits using the eccentricity"""
-        if e >= 1:
+        """Calculate the true anomaly of the asymptote for open orbits using the eccentricity"""
+        if not self._is_closed:
             return np.arccos(-1/e)
 
         return np.nan
 
     def _turning_angle_e(self, e: float) -> float:
-        """Calculate the turning angle for hyperbolic orbits using the eccentricity"""
-        if e >= 1:
+        """Calculate the turning angle for open orbits using the eccentricity"""
+        if not self._is_closed:
             return 2*np.arcsin(1/e)
 
         return np.nan
 
-    def _aiming_radius_erp(self, e: float, b: float) -> float:
-        if e > 1:
+    def _aiming_radius_erp(self, b: float) -> float:
+        """Calculate the aiming radius for hyperbolic orbits using the semi-minor axis"""
+        if self._orbit_type == "hyperbolic":
             return -b
 
         return np.nan
