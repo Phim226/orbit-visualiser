7dfffec043cb1cd928970b1cf0d888865f2d867b
Andrew Phimister <157378252+Phim226@users.noreply.github.com>
2026-02-08 04:54:54 +0000

    Moved specs into separate common file

---
+++ src/orbit_visualiser/ui/common/specs.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from typing import Callable, TypeVar, Generic, TYPE_CHECKING
+from dataclasses import dataclass
+if TYPE_CHECKING:
+    from orbit_visualiser.core import Orbit, Satellite, CentralBody
+
+T = TypeVar("T")
+
+@dataclass(frozen = True)
+class PropertySpec(Generic[T]):
+    label: str
+    obj: Orbit | Satellite | CentralBody
+    units: str | None
+    getter: Callable[[T], float | bool]
+
+@dataclass(frozen = True)
+class VariableSpec(PropertySpec):
+    init_value: float
+    slider_lims: tuple[int]
+    decimal_places: int
+    entry_pos: tuple[int]
