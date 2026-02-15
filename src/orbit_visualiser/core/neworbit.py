deb5fb9ec23d8ad2a2e56aeab87b361686f58f6a
Andrew Phimister <andyph1@hotmail.com>
2026-02-15 21:06:27 +0000

    Orbit refactored into a dataclass

--- src/orbit_visualiser/core/neworbit.py
+++ src/orbit_visualiser/core/neworbit.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import cached_property
 from typing import Sequence
 import numpy as np
 from numpy.typing import NDArray
@@ -11,6 +12,7 @@ from orbit_visualiser.core.astrodynamics.keplerian.elements import (eccentricity
 from orbit_visualiser.core.astrodynamics.keplerian.dynamics import (specific_orbital_energy, characteristic_energy,
                                                                     excess_velocity, specific_ang_momentum_from_state)
 from orbit_visualiser.core.astrodynamics.keplerian.classification import orbit_type
+from orbit_visualiser.core.astrodynamics.types import OrbitType
 
 @dataclass
 class CentralBody():
@@ -27,7 +29,7 @@ class CentralBody():
     mu: float = 398600.0
     r: float = 6378.0
 
-
+@dataclass
 class NewOrbit():
     """
     Represents the instantaneous analytical orbit of a satellite. If there are no perturbations
@@ -37,9 +39,9 @@ class NewOrbit():
 
     Parameters
     ----------
-    pos : Sequence | NDArray[np.float64]
+    position : Sequence | NDArray[np.float64]
         The perifocal position vector of the satellite (km)
-    vel : Sequence | NDArray[np.float64]
+    velocity : Sequence | NDArray[np.float64]
         The perifocal velocity vector of the satellite (km/s)
     mu : float
         The gravitational parameter of the central body (km^3/s^2)
@@ -59,34 +61,76 @@ class NewOrbit():
     nu : float
         The true anomaly of the satellite (rads)
     """
+    position : Sequence | NDArray[np.float64]
+    velocity : Sequence | NDArray[np.float64]
+    mu : float
 
-    def __init__(
-            self,
-            pos: Sequence | NDArray[np.float64],
-            vel: Sequence | NDArray[np.float64],
-            mu: float
-    ):
-        self._pos = pos
-        self._vel = vel
-        self._mu = mu
+    @cached_property
+    def eccentricity(self) -> float:
+        return eccentricity_from_state(self.position, self.velocity, self.mu)
 
-        self.compute_orbital_elements(pos, vel, mu)
+    @cached_property
+    def true_anomaly(self) -> float:
+        return true_anomaly_from_state(self.position)
 
-    @property
-    def position(self) -> Sequence | NDArray[np.float64]:
-        return self._pos
+    @cached_property
+    def orbit_type(self) -> OrbitType:
+        return orbit_type(self.eccentricity)
 
-    @property
-    def velocity(self) -> Sequence | NDArray[np.float64]:
-        return self._vel
+    @cached_property
+    def semi_parameter(self) -> OrbitType:
+        return semi_parameter_from_momentum(specific_ang_momentum_from_state(self.position, self.velocity), self.mu)
 
-    @property
-    def gravitational_parameter(self) -> float:
-        return self._mu
+    @cached_property
+    def radius_of_periapsis(self) -> OrbitType:
+        return periapsis(self.semi_parameter, self.eccentricity)
 
+    @cached_property
+    def semimajor_axis(self) -> OrbitType:
+        return semimajor_axis(self.orbit_type, self.eccentricity, self.radius_of_periapsis)
+
+    @cached_property
+    def semiminor_axis(self) -> OrbitType:
+        return semiminor_axis(self.orbit_type, self.eccentricity, self.semimajor_axis)
+
+    @cached_property
+    def radius_of_apoapsis(self) -> OrbitType:
+        return apoapsis(self.orbit_type, self.eccentricity, self.semimajor_axis)
+
+    @cached_property
+    def asymptote_anomaly(self) -> OrbitType:
+        return asymptote_anomaly(self.orbit_type, self.eccentricity)
+
+    @cached_property
+    def turning_angle(self) -> OrbitType:
+        return turning_angle(self.orbit_type, self.eccentricity)
+
+    @cached_property
+    def aiming_radius(self) -> OrbitType:
+        return aiming_radius(self.orbit_type, self.semiminor_axis)
+
+    @cached_property
+    def orbital_period(self) -> OrbitType:
+        return orbital_period(self.orbit_type, self.mu, self.semimajor_axis)
+
+    @cached_property
+    def mean_motion(self) -> OrbitType:
+        return mean_motion(self.orbit_type, self.orbital_period, self.mu, self.semi_parameter, self.semimajor_axis)
+
+    @cached_property
+    def specific_energy(self) -> OrbitType:
+        return specific_orbital_energy(self.orbit_type, self.mu, self.semimajor_axis)
+
+    @cached_property
+    def characteristic_energy(self) -> OrbitType:
+        return characteristic_energy(self.orbit_type, self.mu, self.semimajor_axis)
+
+    @cached_property
+    def hyperbolic_excess_velocity(self) -> OrbitType:
+        return excess_velocity(self.orbit_type, self.mu, self.semimajor_axis)
 
     @classmethod
-    def from_orbital_elements(cls, e: float, rp: float, mu: float, nu: float):
+    def from_orbital_elements(cls, e: float, rp: float, mu: float, nu: float, asymptote_anomaly: float):
         """
         Alternative constructor for the Orbit class. Takes the orbital elements eccentricity,
         radius of perapsis, the gravitational parameter and the true anomaly as arguments.
@@ -107,41 +151,7 @@ class NewOrbit():
         Orbit
             A new instance of Orbit
         """
-        r, v = state_pf_from_e_rp(e, rp, mu, nu)
+        r, v = state_pf_from_e_rp(e, rp, mu, nu, asymptote_anomaly)
         return cls(r, v, mu)
 
-    def compute_orbital_elements(
-            self,
-            pos: Sequence | NDArray[np.float64],
-            vel: Sequence | NDArray[np.float64],
-            mu: float
-    ) -> None:
-        """
-        Computes the orbital elements, including geometric and energy invariants.
-
-        Parameters
-        ----------
-        pos : Sequence | NDArray[np.float64]
-            The perifocal position vector of the satellite (km)
-        vel : Sequence | NDArray[np.float64]
-            The perifocal velocity vector of the satellite (km/s)
-        mu : float
-            The gravitational parameter of the central body (km^3/s^2)
-        """
-        self.eccentricity = eccentricity_from_state(pos, vel, mu)
-        self.true_anomaly = true_anomaly_from_state(pos)
-        self.orbit_type = orbit_type(self.eccentricity)
-        self.semi_parameter = semi_parameter_from_momentum(specific_ang_momentum_from_state(pos, vel), mu)
-        self.radius_of_periapsis = periapsis(self.semi_parameter, self.eccentricity)
-        self.semimajor_axis = semimajor_axis(self.orbit_type, self.eccentricity, self.radius_of_periapsis)
-        self.semiminor_axis = semiminor_axis(self.orbit_type, self.eccentricity, self.semimajor_axis)
-        self.radius_of_apoapsis = apoapsis(self.orbit_type, self.eccentricity, self.semimajor_axis)
-        self.asymptote_anomaly = asymptote_anomaly(self.orbit_type, self.eccentricity)
-        self.turning_angle = turning_angle(self.orbit_type, self.eccentricity)
-        self.aiming_radius = aiming_radius(self.orbit_type, self.semiminor_axis)
-        self.orbital_period = orbital_period(self.orbit_type, mu, self.semimajor_axis)
-        self.mean_motion = mean_motion(self.orbit_type, self.orbital_period, mu, self.semi_parameter, self.semimajor_axis)
-        self.specific_energy = specific_orbital_energy(self.orbit_type, mu, self.semimajor_axis)
-        self.characteristic_energy = characteristic_energy(self.orbit_type, mu, self.semimajor_axis)
-        self.hyperbolic_excess_velocity = excess_velocity(self.orbit_type, mu, self.semimajor_axis)
 
