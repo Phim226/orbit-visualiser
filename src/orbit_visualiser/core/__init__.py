eb64e383add7289526f94076b4830a559fdf858d
Andrew Phimister <andyph1@hotmail.com>
2026-02-16 04:09:04 +0000

    New helpers and OrbitType can be called from the orbit_visualiser package

--- src/orbit_visualiser/core/__init__.py
+++ src/orbit_visualiser/core/__init__.py
@@ -1,3 +1,8 @@
 from .orbit import Orbit, CentralBody
 from .satellite import Satellite
 from .propagation import get_init_conditions, run_orbit_prop
+
+from .astrodynamics.types import OrbitType
+from .astrodynamics.keplerian.classification import orbit_type
+from .astrodynamics.keplerian.anomlies import mean_anomaly, eccentric_anomaly
+from .astrodynamics.keplerian.elements import asymptote_anomaly
