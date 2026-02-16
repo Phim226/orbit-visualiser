51cda3688120841006fef880603a6fbf029ce0a5
Andrew Phimister <andyph1@hotmail.com>
2026-02-16 21:49:46 +0000

    Wrote dynamics unit tests

--- src/orbit_visualiser/core/__init__.py
+++ src/orbit_visualiser/core/__init__.py
@@ -6,3 +6,6 @@ from .astrodynamics.types import OrbitType
 from .astrodynamics.keplerian.classification import orbit_type
 from .astrodynamics.keplerian.anomlies import mean_anomaly, eccentric_anomaly
 from .astrodynamics.keplerian.elements import asymptote_anomaly
+from .astrodynamics.keplerian.dynamics import (specific_ang_momentum_from_state, specific_ang_momentum,
+                                               specific_orbital_energy, characteristic_energy,
+                                               excess_velocity, vis_viva_speed)
