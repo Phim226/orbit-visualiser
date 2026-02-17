21e3e99dab987fc1843f7dc3e4b99e294e780f55
Andrew Phimister <andyph1@hotmail.com>
2026-02-17 02:10:59 +0000

    Removed unused OrbitType input

--- tests/unit_tests.py/anomalies_test.py
+++ tests/unit_tests.py/anomalies_test.py
@@ -11,7 +11,7 @@ def test_circular_eccentric_anomaly(closed_anomaly_grid: NDArray[np.float64]):
     Tests that the eccentric anomaly is equivalent to the true anomaly for circular orbits.
     """
     nu = closed_anomaly_grid
-    e_anomaly = eccentric_anomaly(OrbitType.CIRCULAR, 0, nu)
+    e_anomaly = eccentric_anomaly(0, nu)
     assert np.allclose(nu, e_anomaly)
 
 @pytest.mark.parametrize("e", e_elliptical_test_cases)
@@ -20,7 +20,7 @@ def test_elliptical_eccentric_anomaly(closed_anomaly_grid: NDArray[np.float64], 
     Tests that the eccentric anomaly for elliptical orbits is equivalent to the expected value.
     """
     nu = closed_anomaly_grid
-    e_anomaly = eccentric_anomaly(OrbitType.ELLIPTICAL, e, nu)
+    e_anomaly = eccentric_anomaly(e, nu)
 
     expected = 2*np.arctan(np.sqrt((1 - e)/(1 + e))*np.tan(nu/2))
     expected = np.where(expected < 0, expected + 2*pi, expected)
@@ -32,7 +32,7 @@ def test_parabolic_eccentric_anomaly(open_anomaly_grid: NDArray[np.float64]):
     Tests that the eccentric anomaly for parabolic orbits is NaN.
     """
     nu = open_anomaly_grid(e = 1)
-    e_anomaly = eccentric_anomaly(OrbitType.PARABOLIC, 1, nu)
+    e_anomaly = eccentric_anomaly(1, nu)
 
     assert np.allclose(e_anomaly, np.full(len(nu), np.nan), equal_nan = True)
 
@@ -42,7 +42,7 @@ def test_hyperbolic_eccentric_anomaly(open_anomaly_grid: NDArray[np.float64], e:
     Tests that the eccentric anomaly for hyperbolic orbits is equivalent to the expected value.
     """
     nu = open_anomaly_grid(e = e)
-    e_anomaly = eccentric_anomaly(OrbitType.HYPERBOLIC, e, nu)
+    e_anomaly = eccentric_anomaly(e, nu)
 
     expected = 2*np.arctanh(np.sqrt((e - 1)/(e + 1))*np.tan(nu/2))
 
@@ -54,8 +54,8 @@ def test_closed_mean_anomaly(closed_anomaly_grid: NDArray[np.float64], e: float)
     Tests that the mean anomaly for closed orbits is equivalent to the expected value.
     """
     nu = closed_anomaly_grid
-    e_anomaly = eccentric_anomaly(OrbitType.CIRCULAR, e, nu)
-    m_anomaly = mean_anomaly(OrbitType.CIRCULAR, e, nu, e_anomaly)
+    e_anomaly = eccentric_anomaly(e, nu)
+    m_anomaly = mean_anomaly(e, nu, e_anomaly)
     expected = e_anomaly - e*np.sin(e_anomaly)
 
     assert np.allclose(m_anomaly, expected)
@@ -65,7 +65,7 @@ def test_parabolic_mean_anomaly(closed_anomaly_grid: NDArray[np.float64]):
     Tests that the mean anomaly for parabolic orbits is equivalent to the expected value.
     """
     nu = closed_anomaly_grid
-    m_anomaly = mean_anomaly(OrbitType.PARABOLIC, 1, nu, np.nan)
+    m_anomaly = mean_anomaly(1, nu, np.nan)
 
     expected = 0.5*np.tan(nu/2) + (1/6)*(np.tan(nu/2))**3
 
@@ -77,8 +77,8 @@ def test_hyperbolic_mean_anomaly(open_anomaly_grid: NDArray[np.float64], e: floa
     Tests that the mean anomaly for hyperbolic orbits is equivalent to the expected value.
     """
     nu = open_anomaly_grid(e = e)
-    e_anomaly = eccentric_anomaly(OrbitType.HYPERBOLIC, e, nu)
-    m_anomaly = mean_anomaly(OrbitType.HYPERBOLIC, e, nu, e_anomaly)
+    e_anomaly = eccentric_anomaly(e, nu)
+    m_anomaly = mean_anomaly(e, nu, e_anomaly)
     expected = e*np.sinh(e_anomaly) - e_anomaly
 
     assert np.allclose(m_anomaly, expected)
