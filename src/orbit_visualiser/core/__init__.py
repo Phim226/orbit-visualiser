bb6b1ed352298963214461d6cd0afca842119e11
Andrew Phimister <andyph1@hotmail.com>
2026-02-17 00:27:43 +0000

    Included elements helper functions in package

--- src/orbit_visualiser/core/__init__.py
+++ src/orbit_visualiser/core/__init__.py
@@ -5,7 +5,12 @@ from .propagation import get_init_conditions, run_orbit_prop
 from .astrodynamics.types import OrbitType
 from .astrodynamics.keplerian.classification import orbit_type
 from .astrodynamics.keplerian.anomlies import mean_anomaly, eccentric_anomaly
-from .astrodynamics.keplerian.elements import asymptote_anomaly
+from .astrodynamics.keplerian.elements import (eccentricity_vector_from_state, eccentricity_from_state,
+                                               true_anomaly_from_state, semi_parameter_from_momentum,
+                                               semi_parameter_from_eccentricity, semimajor_axis,
+                                               semiminor_axis, periapsis, apoapsis,
+                                               asymptote_anomaly, turning_angle, aiming_radius,
+                                               orbital_period, mean_motion)
 from .astrodynamics.keplerian.dynamics import (specific_ang_momentum_from_state, specific_ang_momentum,
                                                specific_orbital_energy, characteristic_energy,
                                                excess_velocity, vis_viva_speed)
