fa81897b114f0bcb5118fc46c55bf0dc15d518c5
Andrew Phimister <andyph1@hotmail.com>
2026-04-29 00:26:40 +0100

    Created prograde/retrograde enum

--- src/orbit_visualiser/core/astrodynamics/types.py
+++ src/orbit_visualiser/core/astrodynamics/types.py
@@ -8,3 +8,10 @@ class OrbitType(Enum):
     ELLIPTICAL = 2
     PARABOLIC = 3
     HYPERBOLIC = 4
+
+class OrbitMotion(Enum):
+    """
+    Enum representing whether an analytical Keplerian orbit is prograde or retrograde.
+    """
+    PROGRADE = 1
+    RETROGRADE = 2
