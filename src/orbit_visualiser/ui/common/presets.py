2b73863fe9d857f6c3aa28e132122b51e83aead5
Andrew Phimister <andyph1@hotmail.com>
2026-05-01 22:27:30 +0100

    3D Euler angles included in Preset data class

--- src/orbit_visualiser/ui/common/presets.py
+++ src/orbit_visualiser/ui/common/presets.py
@@ -4,8 +4,11 @@ from dataclasses import dataclass
 class Preset():
     eccentricity: float
     radius_of_periapsis: float
-    gravitational_parameter: float
     true_anomaly: float
+    right_ascension_of_the_ascending_node: float
+    inclination: float
+    argument_of_periapsis: float
+    gravitational_parameter: float
     radius: float
 
-initial_config: Preset = Preset(0.0, 50_000.0, 398600.0, 0.0, 6378.0)
+initial_config: Preset = Preset(0.0, 50_000.0, 0.0, 0.0, 0.0, 398600.0, 6378.0)
