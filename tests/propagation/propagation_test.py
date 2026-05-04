24f0619445d330dab7cd3fd781597ab93f7918be
Andrew Phimister <andyph1@hotmail.com>
2026-05-04 02:26:28 +0100

    Removed phase shift test from propagation tests

--- tests/propagation/propagation_test.py
+++ tests/propagation/propagation_test.py
@@ -48,24 +48,3 @@ def test_1p_propagation_pos(
     prop_pos_at_rp = prop.y[:2, -1]
 
     assert np.allclose(pos_at_rp, prop_pos_at_rp, atol = 0.05)
-
-
-@pytest.mark.parametrize("e, rp, mu", typical_closed_test_cases)
-def test_1p_phase_shift(
-        orbit_factory_from_elements: Callable[[float, float, float, float], Orbit],
-        e: float,
-        rp: float,
-        mu: float
-):
-    """
-    Test that the phase shift of a propagated satellite after a single period from periapsis for
-    typical closed orbits is within an acceptable tolerance to the analytical solution.
-    """
-    orbit: Orbit = orbit_factory_from_elements(e, rp, mu, 0.0)
-
-    prop = run_orbit_prop(orbit, orbit.orbital_period)
-
-    prop_pos_at_rp = prop.y[:2, -1]
-    phase_shift = np.arctan2(prop_pos_at_rp[1], prop_pos_at_rp[0])
-
-    assert np.isclose(phase_shift, 0, atol=5e-8)
