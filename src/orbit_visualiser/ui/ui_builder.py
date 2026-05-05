9fe9eba0cc9d4475bde9ad8f190448d4ab7a0b95
Andrew Phimister <157378252+Phim226@users.noreply.github.com>
2026-05-05 16:46:08 +0100

    Geometry manager implemented in ui builder

--- src/orbit_visualiser/ui/ui_builder.py
+++ src/orbit_visualiser/ui/ui_builder.py
@@ -6,43 +6,22 @@ from orbit_visualiser.ui.figure.orbit_figure_builder import OrbitFigureBuilder
 from orbit_visualiser.ui.properties.properties_panel_builder import PropertiesBuilder
 from orbit_visualiser.ui.input.input_panel_builder import InputBuilder
 from orbit_visualiser.ui.data_access import OrbitDataAccess
+from orbit_visualiser.ui.common.geometry import GeometryManager, FrameGeometry
 
 class UIBuilder():
-    INPUT_GEOMETRY: dict[str, str | int | bool] = {
-        "side": "left",
-        "anchor": "nw",
-        "padx": 2,
-        "pady": (2, 0),
-        "fill": "y",
-        "expand": True
-    }
-    FIGURE_GEOMETRY: dict[str, str | int | bool] = {
-        "side": "left",
-        "anchor": "nw",
-        "padx": 8,
-        "pady": 6,
-        "fill": "both",
-        "expand": True
-    }
-    PROPS_GEOMETRY: dict[str, str | int | bool] = {
-        "side": "left",
-        "anchor": "nw",
-        "padx": 0,
-        "pady": (2, 0),
-        "fill": "y",
-        "expand": True
-    }
 
-    def __init__(self, root: Window, oda: OrbitDataAccess):
+    def __init__(self, root: Window, oda: OrbitDataAccess, geo_manager: GeometryManager):
         self._root = root
         self._oda = oda
+        self._geo_manager = geo_manager
 
-        self._input_frame = self._build_frame(UIBuilder.INPUT_GEOMETRY)
-        self._figure_frame = self._build_frame(UIBuilder.FIGURE_GEOMETRY, scrollable = False)
-        self._properties_frame = self._build_frame(UIBuilder.PROPS_GEOMETRY)
+        input_geom, figure_geom, props_geom = geo_manager.parent_frame
+        self._input_frame = self._build_frame(input_geom)
+        self._figure_frame = self._build_frame(figure_geom, scrollable = False)
+        self._properties_frame = self._build_frame(props_geom)
 
-        self._input_builder = InputBuilder(self._input_frame, oda)
-        self._figure_builder = OrbitFigureBuilder(self._figure_frame, oda)
+        self._input_builder = InputBuilder(self._input_frame, oda, geo_manager)
+        self._figure_builder = OrbitFigureBuilder(self._figure_frame, oda, geo_manager)
         self._properties_builder = PropertiesBuilder(self._properties_frame, oda)
 
     @property
@@ -68,11 +47,14 @@ class UIBuilder():
         self._figure_builder.build()
         self._properties_builder.build_properties_frame(format_value)
 
-    def _build_frame(self, frame_geom: dict[str, str | int | bool], scrollable: bool = True) -> Frame | ScrolledFrame:
-        frame = (ScrolledFrame(self._root, width = 315, autohide = True)
+    def _build_frame(self, frame_geom: FrameGeometry, scrollable: bool = True) -> Frame | ScrolledFrame:
+        scroll_frame_geom = self._geo_manager.parent_scrollable
+        frame = (ScrolledFrame(self._root, width = scroll_frame_geom.width, autohide = True,
+                               padding = scroll_frame_geom.padding)
                 if scrollable else Frame(self._root, bd = 1))
-        frame.pack(side = frame_geom["side"], anchor = frame_geom["anchor"], padx = frame_geom["padx"],
-                   pady = frame_geom["pady"], fill = frame_geom["fill"], expand = frame_geom["expand"]
+
+        frame.pack(side = frame_geom.side, anchor = frame_geom.anchor, padx = frame_geom.padx,
+                   pady = frame_geom.pady, fill = frame_geom.fill, expand = frame_geom.expand
                    )
 
         return frame
